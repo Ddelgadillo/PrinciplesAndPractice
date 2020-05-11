cd45431b2dd4f87a620bc8da11d202f2e10af86c
Diego Delgadillo <ddelgadillo11@gmail.com>
2020-05-11 10:05:38 -0500

    Defined ReadNames function

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+#include "NamePairs.h"
+
+int main()
+{
+	NamePairs one;
+
+	one.ReadNames();
+}
--- NamePairs.cpp
+++ NamePairs.cpp
@@ -2,6 +2,20 @@
 
 void NamePairs::ReadNames()
 {
+	char ans;
+	std::string tempName;
+
+	do
+	{
+		std::cout << "Please enter name: " << std::endl;
+		std::cin >> tempName;
+
+		name.push_back(tempName);
+
+		std::cout << "Add another(Y or N)? ";
+		std::cin >> ans;
+
+	} while (ans == 'y' or ans == 'Y');
 }
 
 void NamePairs::ReadAges()
