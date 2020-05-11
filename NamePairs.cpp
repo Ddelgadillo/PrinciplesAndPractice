2111dba543c85bd7756d9a4b060cd83006889f9d
Diego Delgadillo <ddelgadillo11@gmail.com>
2020-05-11 09:31:15 -0500

    Added NamePairs

---
+++ NamePairs.cpp
@@ -0,0 +1,17 @@
+#include "NamePairs.h"
+
+void NamePairs::ReadNames()
+{
+}
+
+void NamePairs::ReadAges()
+{
+}
+
+void NamePairs::Print()
+{
+}
+
+void NamePairs::Sort()
+{
+}
---
+++ NamePairs.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+#include<vector>
+
+class NamePairs
+{
+public:
+	void ReadNames(); //Input name
+	void ReadAges(); //Prompts user for age for each name
+	void Print(); //Prints Names and ages (Name[i], age[i]
+	void Sort(); //Sorts the name vector in alphabetical order and reorganizes the age vector to match.
+private:
+	std::vector<std::string> name;
+	std::vector <double> age;
+};
