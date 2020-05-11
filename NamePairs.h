6925b48414c560f34c2f600bad13713f0736e528
Diego Delgadillo <ddelgadillo11@gmail.com>
2020-05-11 14:17:54 -0500

    Added == overload function and defined it

--- NamePairs.h
+++ NamePairs.h
@@ -9,6 +9,7 @@ public:
 	void ReadAges(); //Prompts user for age for each name
 	void Print(); //Prints Names and ages (Name[i], age[i]
 	void Sort(); //Sorts the name vector in alphabetical order and reorganizes the age vector to match.
+	bool operator==(NamePairs& pairs);
 private:
 	std::vector<std::string> name;
 	std::vector <double> age;
