6dc634fa49185b8bee3e22011a6f1164c8df0620
Diego Delgadillo <ddelgadillo11@gmail.com>
2020-05-13 12:44:16 -0500

    Added output overloaded function

--- NamePairs.h
+++ NamePairs.h
@@ -2,17 +2,20 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<ostream>
 
 class NamePairs
 {
 public:
+	NamePairs() = default;
 	void ReadNames(); //Input name
 	void ReadAges(); //Prompts user for age for each name
-	void Print(); //Prints Names and ages (Name[i], age[i]
 	void Sort(); //Sorts the name vector in alphabetical order and reorganizes the age vector to match.
+	void Print() const; //Prints Names and ages (Name[i], age[i]
 	bool operator==(const NamePairs& pairs) const;
 	bool operator!=(const NamePairs& pairs) const;
-private:
+	friend std::ostream& operator<<(std::ostream& output, const NamePairs& pair);
+protected:
 	std::vector<std::string> name;
 	std::vector <double> age;
 };
